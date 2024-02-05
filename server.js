4fb4bf6165b70cca5b883cb98bd496e942d3bdbe
artificialair <jaybaker964@gmail.com>
2024-02-05 17:06:08 -0500

    added GET endpoint

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 const PORT = 3001;
+const notesData = require('./db/db.json');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -11,7 +17,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    // Logging request to terminal
+    console.info(`${req.method} request received to get notes`);
+
+    // Send all notes to the client
+    return res.status(200).json(notesData);
 });
 
 // TODO: A /api/notes GET endpoint that returns notes as a json
